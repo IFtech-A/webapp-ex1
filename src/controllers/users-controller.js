7845ffd7d977d2e9fcfd4fb676a1eb08efbf2a97
Feruz Iskandarov <feruzbek.iskandarov@gmail.com>
2024-01-16 13:52:49 -0500

    Add validation

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,8 +1,15 @@
 
+const validator = require('validator')
 const usersService = require('../services/users-service')
 
 async function updateBalance(req, res) {
     let { amount } = req.params
+
+    if (!validator.isNumeric(amount)) {
+        return res.status(400).json({
+            error: "Bad Request"
+        })
+    }
 
     amount = parseInt(amount)
 
